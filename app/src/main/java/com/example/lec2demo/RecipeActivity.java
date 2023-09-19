5e387b117a2b4f284945e01e2d2d46153db30cde
Rajapakse <rajapaksea@student.douglascollege.ca>
2023-09-19 09:59:34 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/lec2demo/RecipeActivity.java
@@ -0,0 +1,22 @@
+package com.example.lec2demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+        //Practice Exercise. Review the slides to see how the
+        //recipe activity should look.
+        //In your xml recipe layout, Add txtViewTitle2,
+        //txtViewIngredTitle, txtViewIngred1
+        //etc...txtViewDirecnTitle, txtViewDirecns etc.
+        //set up id, Text and constraints, bias, margins etc.
+
+    }
+}
